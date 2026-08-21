950e5b290f16a4ef65877b6e654547d1362bb984
Miguel <encinaparedes@gmail.com>
2026-08-21 02:19:02 +0200

    Forzar actualizacion de supabaseClient

--- app/supabaseClient.ts
+++ app/supabaseClient.ts
@@ -3,4 +3,4 @@ import { createClient } from '@supabase/supabase-js';
 const supabaseUrl = process.env.NEXT_PUBLIC_SUPABASE_URL || 'https://hozvongacraetwmyipqv.supabase.co';
 const supabaseKey = process.env.NEXT_PUBLIC_SUPABASE_ANON_KEY || 'sb_publishable_Vv9XycgQnSMRvqQ0e8EeGQ_4w71DYqb';
 
-export const supabase = createClient(supabaseUrl, supabaseKey);
+export const supabase = createClient(supabaseUrl, supabaseKey); 
